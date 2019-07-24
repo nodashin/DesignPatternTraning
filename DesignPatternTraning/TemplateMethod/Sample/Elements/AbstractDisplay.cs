46eac1feb3803328f71bf45bd8e7e9581b573a2d
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-07-24 13:39:36 +0900

    TemplateMethodパターン

---
+++ DesignPatternTraning/TemplateMethod/Sample/Elements/AbstractDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod.Sample.Elements
+{
+    /// <summary>
+    /// テンプレートクラス
+    /// </summary>
+    abstract class AbstractDisplay
+    {
+        /// <summary>
+        /// サブクラスに実装を任せる抽象メソッド
+        /// </summary>
+        public abstract void Open();
+
+        /// <summary>
+        /// サブクラスに実装を任せる抽象メソッド
+        /// </summary>
+        public abstract void Print();
+
+        /// <summary>
+        /// サブクラスに実装を任せる抽象メソッド
+        /// </summary>
+        public abstract void Close();
+
+        /// <summary>
+        /// テンプレートメソッド
+        /// </summary>
+        public void Display()
+        {
+            this.Open();
+            for (int i = 0; i < 5; i++)
+            {
+                this.Print();
+            }
+            this.Close();
+        }
+    }
+}
