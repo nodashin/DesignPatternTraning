42fea0f9e69eeaac48944f4ecb28fa0b39c99bbc
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-07-25 17:12:28 +0900

    Singletonパターン。

---
+++ DesignPatternTraning/Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("サンプルプログラム");
+            Sample.Main.Execute();
+            Console.WriteLine("");
+
+            Console.WriteLine("練習問題5-1");
+            Traning1.Traning1.Execute();
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatternTraning/Singleton/Traning1/Traning1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Singleton.Traning1.Elements;
+
+namespace Singleton.Traning1
+{
+    static class Traning1
+    {
+        public static void Execute()
+        {
+            var tm1 = TicketMaker.GetTicketMaker();
+            var tm2 = TicketMaker.GetTicketMaker();
+            Console.WriteLine(tm1.GetNextTicketNumber());
+            Console.WriteLine(tm2.GetNextTicketNumber());
+        }
+    }
+}
