d3a1c66895b83a2944bb282508d42f3846339e46
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-11 17:31:05 +0900

    新規作成。

---
+++ DesignPatternTraning/FactoryMethod/Framework/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.Framework
+{
+    /// <summary>
+    /// 製品
+    /// </summary>
+    abstract class Product
+    {
+        /// <summary>
+        /// 使う。
+        /// </summary>
+        public abstract void Use();
+    }
+}
