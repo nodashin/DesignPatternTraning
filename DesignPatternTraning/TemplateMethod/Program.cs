3f8c14ab01a041b12d015bcf77a005cdddf9727b
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-11 16:20:57 +0900

    新規作成。

--- DesignPatternTraning/TemplateMethod/Program.cs
+++ DesignPatternTraning/TemplateMethod/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 
 namespace TemplateMethod
 {
@@ -10,6 +11,11 @@ namespace TemplateMethod
     {
         static void Main(string[] args)
         {
+            ConsoleWriter.WriteStart("Sample");
+            Sample.Main.Execute();
+            ConsoleWriter.WriteEnd("Sample");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ DesignPatternTraning/TemplateMethod/Sample/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod.Sample
+{
+    class Main
+    {
+        public static void Execute()
+        {
+            //'H' を持ったCharDisplayのインスタンスを1個作る。
+            AbstractDisplay d1 = new CharDisplay('H');
+            //"Hello, World" を持ったStringDisplayのインスタンスを1個作る。
+            AbstractDisplay d2 = new StringDisplay("Hello, World");
+            //"こんにちは。" を持ったStringDisplayのインスタンスを1個作る。
+            AbstractDisplay d3 = new StringDisplay("こんにちは。");
+
+            //d1, d2, d3とも、すべて同じAbstractDisplayのサブクラスのインスタンスだから、継承したDisplayメソッドを呼び出すことができる。
+            //実際の動作は個々のクラスCharDisplayやStringDisplayで定まる。
+            d1.Display();
+            d2.Display();
+            d3.Display();
+        }
+    }
+}
