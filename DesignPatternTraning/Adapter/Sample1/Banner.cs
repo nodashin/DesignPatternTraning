45aa15afa9eb253bea36febde7380798c532d9a5
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-11 12:59:09 +0900

    新規作成。

---
+++ DesignPatternTraning/Adapter/Sample1/Banner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter.Sample1
+{
+    /// <summary>
+    /// 予め用意されているクラス
+    /// 指定した文字列に括弧やアスタリスクを加える
+    /// </summary>
+    class Banner
+    {
+        /// <summary>
+        /// 書き込む文字列
+        /// </summary>
+        private string @string;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="string">書き込む文字列</param>
+        public Banner(string @string)
+        {
+            this.@string = @string;
+        }
+
+        /// <summary>
+        /// 指定した文字列に括弧を加えて表示する。
+        /// </summary>
+        public void ShowWirhParen()
+        {
+            Console.WriteLine("(" + @string + ")");
+        }
+
+        /// <summary>
+        /// 指定した文字列にアスタリスクを加えて表示する。
+        /// </summary>
+        public void ShowWithAster()
+        {
+            Console.WriteLine("*" + @string + "*");
+        }
+    }
+}
