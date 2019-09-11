f6804516d914165f32ae85f5c8ebd91261116ae6
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-11 17:39:31 +0900

    新規作成。

---
+++ DesignPatternTraning/FactoryMethod/IdCard/IDCard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryMethod.Framework;
+
+namespace FactoryMethod.IdCard
+{
+    /// <summary>
+    /// 認識番号カード
+    /// </summary>
+    class IDCard : Product
+    {
+        /// <summary>
+        /// オーナー
+        /// </summary>
+        private string owner;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="owner">オーナー</param>
+        public IDCard(string owner)
+        {
+            Console.WriteLine(owner + "のカードを作ります。");
+            this.owner = owner;
+        }
+
+        /// <summary>
+        /// 使う。
+        /// </summary>
+        public override void Use()
+        {
+            Console.WriteLine(owner + "のカードを使います。");
+        }
+
+        /// <summary>
+        /// オーナーを取得する。
+        /// </summary>
+        /// <returns>オーナー</returns>
+        public string GetOwner()
+        {
+            return owner;
+        }
+    }
+}
