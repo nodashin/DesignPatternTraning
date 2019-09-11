e0ec57f99e6d8dd03ab436b595e71465be99ef6f
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-11 13:00:30 +0900

    新規作成。

---
+++ DesignPatternTraning/Adapter/Sample1/IPrint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter.Sample1
+{
+    /// <summary>
+    /// 必要とされているインタフェース
+    /// </summary>
+    interface IPrint
+    {
+        /// <summary>
+        /// 文字列を弱く表示する。
+        /// </summary>
+        void PrintWeek();
+
+        /// <summary>
+        /// 文字列を強く表示する。
+        /// </summary>
+        void PrintStrong();
+    }
+}
