f9e637fe4b82f2f45fe4bb8370ef77de559ef520
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-11 09:16:42 +0900

    インタフェース作成。

---
+++ DesignPatternTraning/Iterator/Sample/IAggregate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator.Sample
+{
+    /// <summary>
+    /// 集合体を表すインタフェース
+    /// </summary>
+    interface IAggregate
+    {
+        /// <summary>
+        /// 集合体に対応するIteratorを1個作成する。
+        /// </summary>
+        /// <returns>集合体に対応するIterator</returns>
+        IIterator Iterator();
+    }
+}
---
+++ DesignPatternTraning/Iterator/Sample/IIterator.cs
@@ -0,0 +1,22 @@
+﻿namespace Iterator.Sample
+{
+    /// <summary>
+    /// 数え上げ、スキャンを行うインタフェース
+    /// </summary>
+    interface IIterator
+    {
+        /// <summary>
+        /// 「次の要素」が存在するかを調べる。
+        /// ループの終了条件で使う。
+        /// </summary>
+        /// <returns>次の要素が存在したらtrue、次の要素が存在しなかったら(最後の要素までたどり着いていたら)falseを返す。</returns>
+        bool HasNext();
+
+        /// <summary>
+        /// 「次の要素」を得る。
+        /// ちゃんと次の要素を返すように、内部状態を次に進めておく。
+        /// </summary>
+        /// <returns>「次の要素」</returns>
+        object Next();
+    }
+}
