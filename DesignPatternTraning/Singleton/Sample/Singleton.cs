c48342baa0e41d0993c2d990e65aa4521d1881c3
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-12 10:23:40 +0900

    新規作成。

---
+++ DesignPatternTraning/Singleton/Sample/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Sample
+{
+    /// <summary>
+    /// シングルトン
+    /// </summary>
+    class Singleton
+    {
+        /// <summary>
+        /// シングルトンのインスタンス
+        /// </summary>
+        private static Singleton singleton = new Singleton();
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <remarks>
+        /// 外部から new できないように、privateに制限している。
+        /// </remarks>
+        private Singleton()
+        {
+            Console.WriteLine("インスタンスを生成しました。");
+        }
+
+        /// <summary>
+        /// シングルトンクラスのインスタンスを取得するメソッド。
+        /// </summary>
+        /// <returns>シングルトンクラスのインスタンス</returns>
+        public static Singleton GetInstance()
+        {
+            return singleton;
+        }
+    }
+}
