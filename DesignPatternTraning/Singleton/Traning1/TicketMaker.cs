fa4c0069d101c95dc1f7c57eeb4d05589fe5310c
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-12 10:46:02 +0900

    新規作成。

---
+++ DesignPatternTraning/Singleton/Traning1/TicketMaker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Traning1
+{
+    /// <summary>
+    /// チケットメーカー
+    /// </summary>
+    class TicketMaker
+    {
+        /// <summary>
+        /// チケットNo.
+        /// </summary>
+        static int TicketNumber { get; set; } = 1000;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        private TicketMaker() { }
+
+        /// <summary>
+        /// 次のチケットNo.を取得する。
+        /// </summary>
+        /// <returns>チケットNo</returns>
+        public int GetNextTicketNumber()
+            => TicketMaker.TicketNumber++;
+
+        /// <summary>
+        /// チケットメーカーを取得する。
+        /// </summary>
+        /// <returns>チケットメーカー</returns>
+        public static TicketMaker GetTicketMaker()
+            => new TicketMaker();
+    }
+}
