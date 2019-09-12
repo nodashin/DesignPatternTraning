5430e0573c0b3b21040c611283885e607be793c1
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-12 11:45:14 +0900

    新規作成。

---
+++ DesignPatternTraning/Prototype/Framework/IProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.Framework
+{
+    /// <summary>
+    /// 抽象メソッド Use と CleateClone が宣言されているインタフェース
+    /// </summary>
+    interface IProduct : ICloneable
+    {
+        /// <summary>
+        /// 使う。
+        /// 「使う」が何を意味するかは、サブクラスの実装にまかされている。
+        /// </summary>
+        /// <param name="s">引数</param>
+        void User(string s);
+
+        /// <summary>
+        /// インスタンスの複製を行うためのもの。
+        /// </summary>
+        /// <returns>複製したインタフェース</returns>
+        IProduct CreateClone();
+    }
+}
