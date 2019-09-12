f958293bf64f17adb5d2b0fec3799e21593c97d7
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-12 18:39:10 +0900

    新規作成。

---
+++ DesignPatternTraning/Builder/Traning1/Director.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Traning1
+{
+    /// <summary>
+    /// ディレクター
+    /// </summary>
+    class Director
+    {
+        /// <summary>
+        /// ドキュメントビルダー
+        /// </summary>
+        private IDocumentBuilder DocumentBuilder { get; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="documentBuilder">ドキュメントビルダ</param>
+        public Director(IDocumentBuilder documentBuilder)
+        {
+            this.DocumentBuilder = documentBuilder;
+        }
+
+        /// <summary>
+        /// 文書を作成する。
+        /// </summary>
+        /// <returns>ドキュメントビルダーの戻り値</returns>
+        public string Construct()
+        {
+            this.DocumentBuilder.WriteTitle("Greeting");
+
+            this.DocumentBuilder.WriteString("朝から昼にかけて");
+            this.DocumentBuilder.WriteItems("おはようございます。", "こんにちは。");
+
+            this.DocumentBuilder.WriteString("夜に");
+            this.DocumentBuilder.WriteItems("こんばんは。", "おやすみなさい。", "さようなら。");
+
+            return this.DocumentBuilder.Complete();
+        }
+    }
+}
---
+++ DesignPatternTraning/Builder/Traning1/IDocumentBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Traning1
+{
+    /// <summary>
+    /// ドキュメントビルダー
+    /// </summary>
+    interface IDocumentBuilder
+    {
+        /// <summary>
+        /// タイトルを書き込む。
+        /// </summary>
+        /// <param name="title">書き込むタイトル</param>
+        void WriteTitle(string title);
+
+        /// <summary>
+        /// 文字列を書き込むする。
+        /// </summary>
+        /// <param name="string">書き込む文字列</param>
+        void WriteString(string @string);
+
+        /// <summary>
+        /// 箇条書きを書き込む。
+        /// </summary>
+        /// <param name="items">書き込む箇条書きの項目</param>
+        void WriteItems(params string[] items);
+
+        /// <summary>
+        /// 完了する。
+        /// </summary>
+        /// <returns>
+        /// 実装クラスによる。
+        /// </returns>
+        string Complete();
+    }
+}
