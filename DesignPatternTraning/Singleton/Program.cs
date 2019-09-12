6c39dc3bf52117f52aa29e40fdac761ce411afcc
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-12 10:26:44 +0900

    Sample.Mainを実行する処理を追加。

--- DesignPatternTraning/Singleton/Program.cs
+++ DesignPatternTraning/Singleton/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 
 namespace Singleton
 {
@@ -10,6 +11,11 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+            ConsoleWriter.WriteStart("Sample");
+            Sample.Main.Execute();
+            ConsoleWriter.WriteEnd("Sample");
+
+            Console.ReadKey();
         }
     }
 }
