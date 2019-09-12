61394bf3b22c0b6c6af2b2ef101d0a3d46d2bd66
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-12 10:25:24 +0900

    新規作成。

---
+++ DesignPatternTraning/Singleton/Sample/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Sample
+{
+    class Main
+    {
+        public static void Execute()
+        {
+            Singleton obj1 = Singleton.GetInstance();
+            Singleton obj2 = Singleton.GetInstance();
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("obj1とobj2は同じインスタンスです。");
+            }
+            else
+            {
+                Console.WriteLine("obj1とobj2は同じインスタンスでがありません。");
+            }
+        }
+    }
+}
