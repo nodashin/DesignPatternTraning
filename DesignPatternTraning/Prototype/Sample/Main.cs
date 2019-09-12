04f2f351a63c34ed34a38b04e2965fd4536987bd
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-12 13:23:15 +0900

    サンプル完了。

---
+++ DesignPatternTraning/Prototype/Sample/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prototype.Sample.Framework;
+
+namespace Prototype.Sample
+{
+    class Main
+    {
+        public static void Execute()
+        {
+            //準備
+            Manager manager = new Manager();
+            UnderLinePen upen = new UnderLinePen('~');
+            MessageBox mbox = new MessageBox('*');
+            MessageBox sbox = new MessageBox('/');
+            manager.Register("strong message", upen);
+            manager.Register("warning box", mbox);
+            manager.Register("slash box", sbox);
+
+            //生成
+            IProduct p1 = manager.Create("strong message");
+            p1.Use("Hello, world.");
+            IProduct p2 = manager.Create("warning box");
+            p2.Use("Hello, world.");
+            IProduct p3 = manager.Create("slash box");
+            p3.Use("Hello, world.");
+        }
+    }
+}
