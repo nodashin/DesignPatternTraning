f3bbdf406fb3c0284f6ec56550a2c0812d107d47
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-13 12:47:38 +0900

    新規作成。

---
+++ DesignPatternTraning/AbstractFactory/Sample/TableFactory/TableLink.cs
@@ -0,0 +1,28 @@
+﻿using AbstractFactory.Sample.Factory;
+
+namespace AbstractFactory.Sample.TableFactory
+{
+    /// <summary>
+    /// Linkクラスのサブクラス
+    /// </summary>
+    internal class TableLink : Link
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="caption">見出し</param>
+        /// <param name="url">リンクのURL</param>
+        public TableLink(string caption, string url) : base(caption, url)
+        {
+        }
+
+        /// <summary>
+        /// HTMLを作成する。
+        /// </summary>
+        /// <returns>作成したHTML</returns>
+        public override string MakeHTML()
+        {
+            return "<td><a href=\"" + url + "\">" + caption + "</a></td>\n";
+        }
+    }
+}
