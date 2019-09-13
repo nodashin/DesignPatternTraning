4b3a83cb4656cdedb4f4ae4677237cecdc120f5e
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-13 11:03:04 +0900

    新規作成。

---
+++ DesignPatternTraning/AbstractFactory/Sample/Factory/Tray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Sample.Factory
+{
+    /// <summary>
+    /// 複数のLinkやTrayを集めてひとまとまりにしたもの
+    /// </summary>
+    abstract class Tray : Item
+    {
+        /// <summary>
+        /// 追加された項目
+        /// </summary>
+        protected List<Item> tray = new List<Item>();
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="caption">見出し</param>
+        protected Tray(string caption) : base(caption)
+        {
+        }
+
+        /// <summary>
+        /// このクラスに項目を追加する。
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(Item item)
+        {
+            tray.Add(item);
+        }
+    }
+}
