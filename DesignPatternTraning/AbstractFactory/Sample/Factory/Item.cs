96f0358eb6e0fc2cf60819845e99b6bd646a6d2a
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-13 10:41:28 +0900

    新規作成。

---
+++ DesignPatternTraning/AbstractFactory/Sample/Factory/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Sample.Factory
+{
+    /// <summary>
+    /// 項目
+    /// LinkとTrayのスーパークラス
+    /// </summary>
+    public abstract class Item
+    {
+        /// <summary>
+        /// 項目の見出し
+        /// </summary>
+        protected string caption;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="caption">項目の見出し</param>
+        protected Item(string caption)
+        {
+            this.caption = caption;
+        }
+
+        /// <summary>
+        /// HTMLを作成する。
+        /// </summary>
+        /// <returns>作成したHTML</returns>
+        public abstract string MakeHTML();
+    }
+}
