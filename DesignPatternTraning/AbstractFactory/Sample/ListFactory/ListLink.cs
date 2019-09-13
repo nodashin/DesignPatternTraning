116fc3b7fc55a6a83930844e323eb8479771321e
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-13 12:03:08 +0900

    新規作成。

---
+++ DesignPatternTraning/AbstractFactory/Sample/ListFactory/ListLink.cs
@@ -0,0 +1,28 @@
+﻿using AbstractFactory.Sample.Factory;
+
+namespace AbstractFactory.Sample.ListFactory
+{
+    /// <summary>
+    /// Linkクラスのサブクラス
+    /// </summary>
+    internal class ListLink : Link
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="caption">見出し</param>
+        /// <param name="url">リンクのURL</param>
+        public ListLink(string caption, string url) : base(caption, url)
+        {
+        }
+
+        /// <summary>
+        /// HTMLを作成する。
+        /// </summary>
+        /// <returns>作成したHTML</returns>
+        public override string MakeHTML()
+        {
+            return "  <li><a href=\"" + url + "\">" + caption + "</a><li>\n";
+        }
+    }
+}
---
+++ DesignPatternTraning/AbstractFactory/Sample/ListFactory/ListTray.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using AbstractFactory.Sample.Factory;
+
+namespace AbstractFactory.Sample.ListFactory
+{
+    /// <summary>
+    /// Trayクラスのサブクラス
+    /// </summary>
+    internal class ListTray : Tray
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="caption">見出し</param>
+        public ListTray(string caption) : base(caption)
+        {
+        }
+
+        /// <summary>
+        /// HTMLを作成する。
+        /// </summary>
+        /// <returns>作成したHTML</returns>
+        public override string MakeHTML()
+        {
+            StringBuilder buffer = new StringBuilder();
+            buffer.Append("<li>\n");
+            buffer.Append(caption +  "\n");
+            buffer.Append("<ul>\n");
+            foreach (var item in tray)
+            {
+                buffer.Append(item.MakeHTML());
+            }
+            buffer.Append("</ul>\n");
+            buffer.Append("</li>\n");
+
+            return buffer.ToString();
+        }
+    }
+}
