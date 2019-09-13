dc842b04d824e7f1fdd0fc3ab46dc938ed748462
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-13 12:09:42 +0900

    サンプルソースを呼ぶように修正。

--- DesignPatternTraning/AbstractFactory/Program.cs
+++ DesignPatternTraning/AbstractFactory/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 
 namespace AbstractFactory
 {
@@ -10,6 +11,12 @@ namespace AbstractFactory
     {
         static void Main(string[] args)
         {
+            var writer = new ConsoleWriter("Sample");
+            writer.WriteStart();
+            Sample.Main.Execute();
+            writer.WriteEnd();
+
+            ConsoleWriter.WriteDelimiter();
         }
     }
 }
