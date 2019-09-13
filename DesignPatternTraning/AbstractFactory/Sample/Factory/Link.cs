8a04fd7942bd08a155e2aed8986ed03edcf783fc
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-13 10:59:24 +0900

    新規作成。

---
+++ DesignPatternTraning/AbstractFactory/Sample/Factory/Link.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Sample.Factory
+{
+    /// <summary>
+    /// HTMLのハイパーリンク
+    /// </summary>
+    abstract class Link : Item
+    {
+        /// <summary>
+        /// リンクのURL
+        /// </summary>
+        protected string url;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="caption">見出し</param>
+        /// <param name="url">リンクのURL</param>
+        protected Link(string caption, string url) : base(caption)
+        {
+            this.url = url;
+        }
+    }
+}
