51e85e415f643933d30b6e26b2a56eda312dd49c
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-14 10:03:55 +0900

    新規作成。

---
+++ DesignPatternTraning/Bridge/Sample/Display.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Sample
+{
+    /// <summary>
+    /// 抽象的な「何かを表示するもの」。
+    /// 「機能のクラス階層」の最上位にあるクラス。
+    /// </summary>
+    class Display
+    {
+        /// <summary>
+        /// Displayクラスの「実装」を表すインスタンス
+        /// </summary>
+        private DisplayImpl impl;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="impl">Displayクラスの「実装」を表すインスタンス</param>
+        public Display(DisplayImpl impl)
+        {
+            this.impl = impl;
+        }
+
+        public void Open()
+        {
+            impl.RawOpen();
+        }
+
+        public void Print()
+        {
+            impl.RawPrint();
+        }
+
+        public void Close()
+        {
+            impl.RawClose();
+        }
+    }
+}
---
+++ DesignPatternTraning/Bridge/Sample/DisplayImpl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bridge.Sample
+{
+    internal class DisplayImpl
+    {
+        internal void RawOpen()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void RawPrint()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void RawClose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
