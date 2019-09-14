0d94ceb7148af01f4be8b1a6688666d2411cf407
nodashin <rin.s.n.0314@Gmail.com>
2019-09-14 21:17:41 +0900

    問題10-1完了。

---
+++ DesignPatternTraning/Strategy/Traning1/Main.cs
@@ -0,0 +1,22 @@
+﻿using Strategy.Sample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Traning1
+{
+    class Main
+    {
+        public static void Execute()
+        {
+            var p = new Player("Traning1", new RandomStrategy(10));
+            for (int i = 0; i < 100; i++)
+            {
+                var h = p.NextHand();
+                Console.WriteLine(h.ToString());
+            }
+        }
+    }
+}
---
+++ DesignPatternTraning/Strategy/Traning1/RandomStrategy.cs
@@ -0,0 +1,53 @@
+﻿using Strategy.Sample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Traning1
+{
+    /// <summary>
+    /// ランダムストラテジ
+    /// </summary>
+    class RandomStrategy : IStrategy
+    {
+        /// <summary>
+        /// 乱数シード
+        /// </summary>
+        public int Seed { get; }
+
+        /// <summary>
+        /// 乱数ジェネレータ
+        /// </summary>
+        Random Random { get; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="seed">乱数シード</param>
+        public RandomStrategy(int seed)
+        {
+            this.Seed = seed;
+            this.Random = new Random(seed);
+        }
+
+        /// <summary>
+        /// 次の手を出す。
+        /// </summary>
+        /// <returns>次の手</returns>
+        public Hand NextHand()
+        {
+            return Hand.GetHand(this.Random.Next(3));
+        }
+
+        /// <summary>
+        /// 勝敗を学ぶ。
+        /// </summary>
+        /// <param name="win">勝利か否か</param>
+        public void Study(bool win)
+        {
+            //何もしない。
+        }
+    }
+}
