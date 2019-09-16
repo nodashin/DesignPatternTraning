60e9704a788ce8efc05d5f2b65576ec8ec9b4955
nodashin <rin.s.n.0314@Gmail.com>
2019-09-16 12:29:09 +0900

    新規作成。

---
+++ DesignPatternTraning/Composite/Sample/FileTreatmebtException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Composite.Sample
+{
+    [Serializable]
+    internal class FileTreatmebtException : Exception
+    {
+        public FileTreatmebtException()
+        {
+        }
+
+        public FileTreatmebtException(string message) : base(message)
+        {
+        }
+
+        public FileTreatmebtException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected FileTreatmebtException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
