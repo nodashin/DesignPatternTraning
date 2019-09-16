e78c79263e1fb195ca7c01da1b9a8fde086b8f19
nodashin <rin.s.n.0314@Gmail.com>
2019-09-16 12:49:57 +0900

    練習問題完了。

--- DesignPatternTraning/Composite/Program.cs
+++ DesignPatternTraning/Composite/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,12 @@ namespace Composite
     {
         static void Main(string[] args)
         {
+            var writer = new ConsoleWriter("Sample");
+            writer.WriteStart();
+            Sample.Main.Execute();
+            writer.WriteEnd();
+
+            Console.ReadKey();
         }
     }
 }
