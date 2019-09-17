1dab041a237c857bfd2467542a1f1e89c78d45e8
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-17 16:14:52 +0900

    新規作成。

---
+++ DesignPatternTraning/Decorator/Sample/Display.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Sample
+{
+    /// <summary>
+    /// 文字列表示用の抽象クラス。
+    /// </summary>
+    abstract class Display
+    {
+        /// <summary>
+        /// 横の文字数を得る。
+        /// </summary>
+        /// <returns>横の文字数</returns>
+        public abstract int GetColumns();
+
+        /// <summary>
+        /// 縦の文字数を得る。
+        /// </summary>
+        /// <returns>縦の文字数</returns>
+        public abstract int GetRows();
+
+        /// <summary>
+        /// 指定した行の文字列を得る。
+        /// </summary>
+        /// <param name="row">得たい文字列の行</param>
+        /// <returns>指定した行の文字列</returns>
+        public abstract string GetRowText(int row);
+
+        /// <summary>
+        /// すべての行の文字列を表示する。
+        /// </summary>
+        public void Show()
+        {
+            for (int i = 0; i < GetRows(); i++)
+            {
+                Console.WriteLine(GetRowText(i));
+            }
+        }
+    }
+}
