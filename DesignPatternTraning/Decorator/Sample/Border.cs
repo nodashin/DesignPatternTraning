8d017cdd591552d821d2f42d84323952f996097d
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-17 16:53:45 +0900

    新規作成。

---
+++ DesignPatternTraning/Decorator/Sample/Border.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Sample
+{
+    /// <summary>
+    /// 「飾り枠」
+    /// </summary>
+    abstract class Border : Display
+    {
+        /// <summary>
+        /// この飾り枠がくるんでいる「中身」を指す。
+        /// </summary>
+        protected Display display;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="display">この飾り枠がくるんでいる「中身」</param>
+        protected Border(Display display)
+        {
+            //インスタンス生成時に「中身」を引数で指定
+            this.display = display;
+        }
+    }
+}
