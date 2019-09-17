88e7a9cfa05955c37511e1305f77407a5d2b637e
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-17 17:30:30 +0900

    新規作成。

---
+++ DesignPatternTraning/Decorator/Sample/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Sample
+{
+    class Main
+    {
+        public static void Execute()
+        {
+            Display b1 = new StringDisplay("Hello, world.");
+            Display b2 = new SideBorder(b1, '#');
+            Display b3 = new FullBorder(b2);
+            b1.Show();
+            b2.Show();
+            b3.Show();
+            Display b4 = new SideBorder(
+                new FullBorder(
+                    new FullBorder(
+                        new SideBorder(
+                            new FullBorder(
+                                new StringDisplay("こんにちは。")
+                                )
+                            , '*')
+                            )
+                        ),
+                    '/');
+            b4.Show();
+        }
+    }
+}
