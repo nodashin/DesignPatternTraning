7c4e29968eab372cbdfbd58d0f929d1d716ffb47
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-17 17:33:35 +0900

    サンプルを呼び出すように修正。

--- DesignPatternTraning/Decorator/Program.cs
+++ DesignPatternTraning/Decorator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 
 namespace Decorator
 {
@@ -10,6 +11,12 @@ namespace Decorator
     {
         static void Main(string[] args)
         {
+            var writer = new ConsoleWriter("Sample");
+            writer.WriteStart();
+            Sample.Main.Execute();
+            writer.WriteEnd();
+
+            Console.ReadKey();
         }
     }
 }
