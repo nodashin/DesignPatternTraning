c063a52bfb50601674013dd50d54e2ad5732571a
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-18 16:32:42 +0900

    サンプルプログラムの実行準備完了。

---
+++ DesignPatternTraning/ChainOfResponsibility/Sample/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility.Sample
+{
+    class Main
+    {
+        public static void Execute()
+        {
+            Support alice = new NoSupport("Alice");
+            Support bob = new LimitSupport("Bob", 100);
+            Support charlie = new SpecialSupport("Charlie", 429);
+            Support diana = new LimitSupport("Diana", 200);
+            Support elmo = new OddSupport("Elmo");
+            Support fred = new LimitSupport("Fred", 300);
+            //連鎖の形成
+            alice.SetNext(bob).SetNext(charlie).SetNext(diana).SetNext(elmo).SetNext(fred);
+            //さまざまなトラブル発生
+            for (int i = 0; i < 500; i+=33)
+            {
+                alice.SupportTrouble(new Trouble(i));
+            }
+        }
+    }
+}
