a79c986781effea7e64532192cadb890e6945da7
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-18 10:11:01 +0900

    Visitor作成(エラー有り)

---
+++ DesignPatternTraning/Visitor/Sample/Visitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor.Sample
+{
+    /// <summary>
+    /// ファイルやディレクトリを訪れる訪問者を表す抽象クラス。
+    /// </summary>
+    public abstract class Visitor
+    {
+        /// <summary>
+        /// Fileを訪問したときにFileクラスが呼び出すメソッド。
+        /// </summary>
+        /// <param name="file">ファイル</param>
+        public abstract void Visit(File file);
+
+        /// <summary>
+        /// Directoryを訪問したときにDirectoryクラスが呼び出すメソッド。
+        /// </summary>
+        /// <param name="directory">Directory</param>
+        public abstract void Visit(Directory directory);
+    }
+}
