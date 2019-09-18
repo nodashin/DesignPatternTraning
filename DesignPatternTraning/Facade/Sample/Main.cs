89b7d56d458c6f8d8d8e4377a45670e36d2e6652
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-18 17:59:14 +0900

    Main作成。

---
+++ DesignPatternTraning/Facade/Sample/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.Sample
+{
+    class Main
+    {
+        public static void Execute()
+        {
+            PageMaker.PageMaker.MakeWelcomePage("hyuki@hyuki.com", "welocome.html");
+        }
+    }
+}
