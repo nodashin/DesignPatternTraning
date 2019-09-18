5aa73d69fec276bd0a419b627da3f34b180dab01
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-18 16:14:19 +0900

    OddSupport作成。

---
+++ DesignPatternTraning/ChainOfResponsibility/Sample/OddSupport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility.Sample
+{
+    /// <summary>
+    /// トラブルを解決する具象クラス。
+    /// 奇数番号のトラブルを解決。
+    /// </summary>
+    class OddSupport : Support
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="name">トラブル解決者の名前</param>
+        public OddSupport(string name) : base(name)
+        {
+        }
+
+        /// <summary>
+        /// 解決用メソッド
+        /// </summary>
+        /// <param name="trouble">解決すべきトラブル</param>
+        /// <returns>tureのときは要求が処理されたことを表し、falseのときには要求はまだ処理されていない(すなわち、次にたらい回しする)ことを表す</returns>
+        protected override bool Resolve(Trouble trouble)
+        {
+            if(trouble.GetNumber() %2 == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
