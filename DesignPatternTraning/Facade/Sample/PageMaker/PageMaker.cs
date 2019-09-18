5eea003cf09b505cc3314831576beae0ed5e3414
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-18 17:57:33 +0900

    PageMaker作成。

---
+++ DesignPatternTraning/Facade/Sample/PageMaker/PageMaker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.Sample.PageMaker
+{
+    /// <summary>
+    /// メールアドレスからユーザーのWebページを作成するクラス
+    /// </summary>
+    class PageMaker
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <remarks>
+        /// インスタンスは作らないのでprivate宣言する。
+        /// </remarks>
+        private PageMaker() { }
+
+        public static void MakeWelcomePage(string mailAddr, string fileName)
+        {
+            Dictionary<string, string> mailProp = Database.GetProperties("maildata");
+            string userName = mailProp[mailAddr];
+            HtmlWriter writer = new HtmlWriter(new System.IO.StreamWriter(fileName));
+            writer.Title("Welcome to " + userName + "'s page!");
+            writer.Paragraph(userName + "のページへようこそ。");
+            writer.Paragraph("メールまっていますね。");
+            writer.MailTo(mailAddr, userName);
+            writer.Close();
+            Console.WriteLine(fileName + " is created for " + mailAddr + " (" + userName + ")");
+        }
+    }
+}
