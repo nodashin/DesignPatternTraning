1721bfe3558ff1d3da4be8a22664fbc4d0914e9a
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-18 10:41:11 +0900

    FileTreatmentException作成。

---
+++ DesignPatternTraning/Visitor/Sample/FileTreatmentException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Visitor.Sample
+{
+    [Serializable]
+    public class FileTreatmentException : Exception
+    {
+        public FileTreatmentException()
+        {
+        }
+
+        public FileTreatmentException(string message) : base(message)
+        {
+        }
+
+        public FileTreatmentException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected FileTreatmentException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
