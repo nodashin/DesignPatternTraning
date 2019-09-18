e2d685baa4c5e6ebc3c261e0f3b453946c6d7a06
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-18 16:10:30 +0900

    LimitSupport作成。

---
+++ DesignPatternTraning/ChainOfResponsibility/Sample/LimitSupport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility.Sample
+{
+    /// <summary>
+    /// トラブルを解決する具象クラス。
+    /// 指定した番号未満のトラブルを解決。
+    /// </summary>
+    class LimitSupport : Support
+    {
+        /// <summary>
+        /// この番号未満なら解決できる。
+        /// </summary>
+        private int limit;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="name">トラブル解決者の名前</param>
+        /// <param name="limit">この番号未満なら解決できる</param>
+        public LimitSupport(string name, int limit) : base(name)
+        {
+            this.limit = limit;
+        }
+
+        /// <summary>
+        /// 解決用メソッド
+        /// </summary>
+        /// <param name="trouble">解決すべきトラブル</param>
+        /// <returns>tureのときは要求が処理されたことを表し、falseのときには要求はまだ処理されていない(すなわち、次にたらい回しする)ことを表す</returns>
+        protected override bool Resolve(Trouble trouble)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
