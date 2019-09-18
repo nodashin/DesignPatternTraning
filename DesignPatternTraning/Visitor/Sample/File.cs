3c05a44a951a3b5fc794139249d3cb0ab5573909
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-18 10:25:59 +0900

    File作成。

---
+++ DesignPatternTraning/Visitor/Sample/File.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor.Sample
+{
+    /// <summary>
+    /// ファイルを表すクラス
+    /// </summary>
+    public class File : Entry
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// サイズ
+        /// </summary>
+        private int size;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="name">名称</param>
+        /// <param name="size">サイズ</param>
+        public File(string name, int size)
+        {
+            this.name = name;
+            this.size = size;
+        }
+
+        /// <summary>
+        /// 名称を取得する。
+        /// </summary>
+        /// <returns>名称</returns>
+        public override string GetName()
+        {
+            return name;
+        }
+
+        /// <summary>
+        /// サイズを取得する。
+        /// </summary>
+        /// <returns>サイズ</returns>
+        public override int GetSize()
+        {
+            return size;
+        }
+
+        /// <summary>
+        /// 訪問者を受け入れる。
+        /// </summary>
+        /// <param name="v">訪問者</param>
+        public override void Accept(Visitor v)
+        {
+            v.Visit(this);
+        }
+    }
+}
