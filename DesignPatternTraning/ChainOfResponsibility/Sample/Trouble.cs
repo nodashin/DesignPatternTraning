e49488044eb2a9beb332c7a462de4832b7c0f0ef
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-18 15:55:47 +0900

    Trouble作成。

---
+++ DesignPatternTraning/ChainOfResponsibility/Sample/Trouble.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility.Sample
+{
+    /// <summary>
+    /// 発生したトラブルを表すクラス。
+    /// </summary>
+    class Trouble
+    {
+        /// <summary>
+        /// トラブル番号
+        /// </summary>
+        private int number;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="number">トラブル番号</param>
+        public Trouble(int number)
+        {
+            this.number = number;
+        }
+
+        /// <summary>
+        /// トラブル番号を取得する。
+        /// </summary>
+        /// <returns>トラブル番号</returns>
+        public int GetNumber()
+        {
+            return number;
+        }
+
+        /// <summary>
+        /// トラブルの文字列表現。
+        /// </summary>
+        /// <returns>トラブルの文字列</returns>
+        public override string ToString()
+        {
+            return "[Trouble" + number + "]"; 
+        }
+    }
+}
