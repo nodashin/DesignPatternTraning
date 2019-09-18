96a6e45c3050c6e0a534762fa7c09b0f14e2f2c8
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-18 17:36:40 +0900

    Database途中まで作成。

---
+++ DesignPatternTraning/Facade/Sample/PageMaker/Database.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.Sample.PageMaker
+{
+    /// <summary>
+    /// メールアドレスからユーザー名を得るクラス。
+    /// </summary>
+    class Database
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <remarks>
+        /// newでインスタンスを生成させないためにprivate宣言。
+        /// </remarks>
+        private Database()
+        {
+            
+        }
+
+        public static Dictionary<string, string> GetProperties(string dbName)
+        {
+            string fileName = dbName + ".txt";
+            Dictionary<string, string> prop = new Dictionary<string, string>();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
