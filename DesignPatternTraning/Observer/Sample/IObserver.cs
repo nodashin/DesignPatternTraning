370970edd1d75f0aaa75a743caea462211f6bb76
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-19 15:50:22 +0900

    IObserver作成。

---
+++ DesignPatternTraning/Observer/Sample/IObserver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer.Sample
+{
+    /// <summary>
+    /// 観測者を表すインタフェース
+    /// </summary>
+    public interface IObserver
+    {
+        void Update(NumberGenerator generator);
+    }
+}
