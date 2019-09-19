0e65737c2b0e54fedf7a50894cae453c24348c89
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-19 16:14:43 +0900

    Main作成。

---
+++ DesignPatternTraning/Observer/Sample/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer.Sample
+{
+    class Main
+    {
+        public static void Execute()
+        {
+            NumberGenerator generator = new RandomNumberGenerator();
+            IObserver observer1 = new DigitObserver();
+            IObserver observer2 = new GraphObserver();
+            generator.AddObserver(observer1);
+            generator.AddObserver(observer2);
+            generator.Exeute();
+        }
+    }
+}
