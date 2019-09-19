a781f5538671f406c7597ae4a0a6fb9669eaa413
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-19 12:03:27 +0900

    物理名設定。

--- DesignPatternTraning/Mediator/Sample/LoginForm.Designer.cs
+++ DesignPatternTraning/Mediator/Sample/LoginForm.Designer.cs
@@ -28,102 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.colleagueRadioButton2 = new Mediator.Sample.ColleagueRadioButton();
-            this.colleagueRadioButton1 = new Mediator.Sample.ColleagueRadioButton();
-            this.colleagueTextBox1 = new Mediator.Sample.ColleagueTextBox();
-            this.colleagueTextBox2 = new Mediator.Sample.ColleagueTextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.colleagueButton1 = new Mediator.Sample.ColleagueButton();
-            this.colleagueButton2 = new Mediator.Sample.ColleagueButton();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.colleagueRadioButtonLogin = new Mediator.Sample.ColleagueRadioButton();
+            this.colleagueRadioButtonGuest = new Mediator.Sample.ColleagueRadioButton();
+            this.colleagueTextBoxUsername = new Mediator.Sample.ColleagueTextBox();
+            this.colleagueTextBoxPassword = new Mediator.Sample.ColleagueTextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.colleagueButtonOK = new Mediator.Sample.ColleagueButton();
+            this.colleagueButtonCancel = new Mediator.Sample.ColleagueButton();
             this.SuspendLayout();
             // 
-            // label1
+            // labelUsername
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 43);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 18);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Username";
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(29, 43);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(68, 18);
+            this.labelUsername.TabIndex = 2;
+            this.labelUsername.Text = "Username";
             // 
-            // colleagueRadioButton2
+            // colleagueRadioButtonLogin
             // 
-            this.colleagueRadioButton2.AutoSize = true;
-            this.colleagueRadioButton2.Location = new System.Drawing.Point(103, 12);
-            this.colleagueRadioButton2.Name = "colleagueRadioButton2";
-            this.colleagueRadioButton2.Size = new System.Drawing.Size(57, 22);
-            this.colleagueRadioButton2.TabIndex = 1;
-            this.colleagueRadioButton2.TabStop = true;
-            this.colleagueRadioButton2.Text = "Login";
-            this.colleagueRadioButton2.UseVisualStyleBackColor = true;
+            this.colleagueRadioButtonLogin.AutoSize = true;
+            this.colleagueRadioButtonLogin.Location = new System.Drawing.Point(103, 12);
+            this.colleagueRadioButtonLogin.Name = "colleagueRadioButtonLogin";
+            this.colleagueRadioButtonLogin.Size = new System.Drawing.Size(57, 22);
+            this.colleagueRadioButtonLogin.TabIndex = 1;
+            this.colleagueRadioButtonLogin.TabStop = true;
+            this.colleagueRadioButtonLogin.Text = "Login";
+            this.colleagueRadioButtonLogin.UseVisualStyleBackColor = true;
             // 
-            // colleagueRadioButton1
+            // colleagueRadioButtonGuest
             // 
-            this.colleagueRadioButton1.AutoSize = true;
-            this.colleagueRadioButton1.Location = new System.Drawing.Point(29, 12);
-            this.colleagueRadioButton1.Name = "colleagueRadioButton1";
-            this.colleagueRadioButton1.Size = new System.Drawing.Size(60, 22);
-            this.colleagueRadioButton1.TabIndex = 0;
-            this.colleagueRadioButton1.TabStop = true;
-            this.colleagueRadioButton1.Text = "Gesut";
-            this.colleagueRadioButton1.UseVisualStyleBackColor = true;
+            this.colleagueRadioButtonGuest.AutoSize = true;
+            this.colleagueRadioButtonGuest.Location = new System.Drawing.Point(29, 12);
+            this.colleagueRadioButtonGuest.Name = "colleagueRadioButtonGuest";
+            this.colleagueRadioButtonGuest.Size = new System.Drawing.Size(60, 22);
+            this.colleagueRadioButtonGuest.TabIndex = 0;
+            this.colleagueRadioButtonGuest.TabStop = true;
+            this.colleagueRadioButtonGuest.Text = "Gesut";
+            this.colleagueRadioButtonGuest.UseVisualStyleBackColor = true;
             // 
-            // colleagueTextBox1
+            // colleagueTextBoxUsername
             // 
-            this.colleagueTextBox1.Location = new System.Drawing.Point(103, 40);
-            this.colleagueTextBox1.Name = "colleagueTextBox1";
-            this.colleagueTextBox1.Size = new System.Drawing.Size(100, 25);
-            this.colleagueTextBox1.TabIndex = 3;
+            this.colleagueTextBoxUsername.Location = new System.Drawing.Point(103, 40);
+            this.colleagueTextBoxUsername.Name = "colleagueTextBoxUsername";
+            this.colleagueTextBoxUsername.Size = new System.Drawing.Size(100, 25);
+            this.colleagueTextBoxUsername.TabIndex = 3;
             // 
-            // colleagueTextBox2
+            // colleagueTextBoxPassword
             // 
-            this.colleagueTextBox2.Location = new System.Drawing.Point(103, 71);
-            this.colleagueTextBox2.Name = "colleagueTextBox2";
-            this.colleagueTextBox2.PasswordChar = '*';
-            this.colleagueTextBox2.Size = new System.Drawing.Size(100, 25);
-            this.colleagueTextBox2.TabIndex = 4;
+            this.colleagueTextBoxPassword.Location = new System.Drawing.Point(103, 71);
+            this.colleagueTextBoxPassword.Name = "colleagueTextBoxPassword";
+            this.colleagueTextBoxPassword.PasswordChar = '*';
+            this.colleagueTextBoxPassword.Size = new System.Drawing.Size(100, 25);
+            this.colleagueTextBoxPassword.TabIndex = 4;
             // 
-            // label2
+            // labelPassword
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 18);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Password";
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(29, 74);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(63, 18);
+            this.labelPassword.TabIndex = 5;
+            this.labelPassword.Text = "Password";
             // 
-            // colleagueButton1
+            // colleagueButtonOK
             // 
-            this.colleagueButton1.Location = new System.Drawing.Point(29, 102);
-            this.colleagueButton1.Name = "colleagueButton1";
-            this.colleagueButton1.Size = new System.Drawing.Size(75, 23);
-            this.colleagueButton1.TabIndex = 6;
-            this.colleagueButton1.Text = "OK";
-            this.colleagueButton1.UseVisualStyleBackColor = true;
+            this.colleagueButtonOK.Location = new System.Drawing.Point(29, 102);
+            this.colleagueButtonOK.Name = "colleagueButtonOK";
+            this.colleagueButtonOK.Size = new System.Drawing.Size(75, 23);
+            this.colleagueButtonOK.TabIndex = 6;
+            this.colleagueButtonOK.Text = "OK";
+            this.colleagueButtonOK.UseVisualStyleBackColor = true;
             // 
-            // colleagueButton2
+            // colleagueButtonCancel
             // 
-            this.colleagueButton2.Location = new System.Drawing.Point(128, 102);
-            this.colleagueButton2.Name = "colleagueButton2";
-            this.colleagueButton2.Size = new System.Drawing.Size(75, 23);
-            this.colleagueButton2.TabIndex = 7;
-            this.colleagueButton2.Text = "Cancel";
-            this.colleagueButton2.UseVisualStyleBackColor = true;
+            this.colleagueButtonCancel.Location = new System.Drawing.Point(128, 102);
+            this.colleagueButtonCancel.Name = "colleagueButtonCancel";
+            this.colleagueButtonCancel.Size = new System.Drawing.Size(75, 23);
+            this.colleagueButtonCancel.TabIndex = 7;
+            this.colleagueButtonCancel.Text = "Cancel";
+            this.colleagueButtonCancel.UseVisualStyleBackColor = true;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(230, 142);
-            this.Controls.Add(this.colleagueButton2);
-            this.Controls.Add(this.colleagueButton1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.colleagueTextBox2);
-            this.Controls.Add(this.colleagueTextBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.colleagueRadioButton2);
-            this.Controls.Add(this.colleagueRadioButton1);
+            this.Controls.Add(this.colleagueButtonCancel);
+            this.Controls.Add(this.colleagueButtonOK);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.colleagueTextBoxPassword);
+            this.Controls.Add(this.colleagueTextBoxUsername);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.colleagueRadioButtonLogin);
+            this.Controls.Add(this.colleagueRadioButtonGuest);
             this.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "LoginForm";
@@ -136,13 +136,13 @@
 
         #endregion
 
-        private ColleagueRadioButton colleagueRadioButton1;
-        private ColleagueRadioButton colleagueRadioButton2;
-        private System.Windows.Forms.Label label1;
-        private ColleagueTextBox colleagueTextBox1;
-        private ColleagueTextBox colleagueTextBox2;
-        private System.Windows.Forms.Label label2;
-        private ColleagueButton colleagueButton1;
-        private ColleagueButton colleagueButton2;
+        private ColleagueRadioButton colleagueRadioButtonGuest;
+        private ColleagueRadioButton colleagueRadioButtonLogin;
+        private System.Windows.Forms.Label labelUsername;
+        private ColleagueTextBox colleagueTextBoxUsername;
+        private ColleagueTextBox colleagueTextBoxPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private ColleagueButton colleagueButtonOK;
+        private ColleagueButton colleagueButtonCancel;
     }
 }
