b767e8be8783a8bf473571cdbd8cfa1b4439addc
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-19 16:16:14 +0900

    サンプルプログラムの呼び出し。

--- DesignPatternTraning/Observer/Program.cs
+++ DesignPatternTraning/Observer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 
 namespace Observer
 {
@@ -10,6 +11,12 @@ namespace Observer
     {
         static void Main(string[] args)
         {
+            var writer = new ConsoleWriter("Sample");
+            writer.WriteStart();
+            Sample.Main.Execute();
+            writer.WriteEnd();
+
+            Console.ReadKey();
         }
     }
 }
