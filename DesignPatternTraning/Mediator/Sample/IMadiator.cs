fa30b44083e10f411be6cb1a7cafc6b1268e4376
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-19 10:32:39 +0900

    IMadiator作成。

---
+++ DesignPatternTraning/Mediator/Sample/IMadiator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator.Sample
+{
+    /// <summary>
+    /// 「相談役」のインタフェース(API)を定めるインタフェース。
+    /// </summary>
+    public interface IMadiator
+    {
+        /// <summary>
+        /// Madiatorが管理するメンバーを生成するメソッド。
+        /// </summary>
+        void CreateColleagues();
+
+        /// <summary>
+        /// 各メンバーであるColleaugeたちから呼び出されるメソッド。
+        /// 相談役への「相談」に相当する。
+        /// </summary>
+        void ColleaguesChanged();
+    }
+}
