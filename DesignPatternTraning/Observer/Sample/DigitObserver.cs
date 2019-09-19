ec5c85154e69f9730b74365ae2329a68aac70687
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-19 16:07:26 +0900

    DigitObserver作成。

--- DesignPatternTraning/Observer/Sample/DigitObserver.cs
+++ DesignPatternTraning/Observer/Sample/DigitObserver.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Observer.Sample
 {
     /// <summary>
-    /// 観測者を表すインタフェース
+    /// 数字で数を表現するクラス
     /// </summary>
-    public interface IObserver
+    public class DigitObserver : IObserver
     {
         /// <summary>
         /// NumberGeneratorが「私の内容が更新されました。表示の方も更新してください」とObserverに伝えるためのメソッド。
         /// </summary>
         /// <param name="generator">NumberGenerator</param>
-        void Update(NumberGenerator generator);
+        public void Update(NumberGenerator generator)
+        {
+            Console.WriteLine("DigitObserver:" + generator.GetNumber());
+            Thread.Sleep(100);
+        }
     }
 }
