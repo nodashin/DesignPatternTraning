dd8e3a134940684324bb6624ca56584d5e028028
nodashin <rin.s.n.0314@Gmail.com>
2019-09-21 00:20:10 +0900

    コンストラクタ作成。

--- DesignPatternTraning/State/Sample/SafeForm.cs
+++ DesignPatternTraning/State/Sample/SafeForm.cs
@@ -10,11 +10,46 @@ using System.Windows.Forms;
 
 namespace State.Sample
 {
-    public partial class SafeForm : Form
+    /// <summary>
+    /// IContextを実装しているクラス。ボタンや画面表示などのユーザーインタフェースを持つ。
+    /// </summary>
+    public partial class SafeForm : Form, IContext
     {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public SafeForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="text">text</param>
+        public SafeForm(string text) : this()
+        {
+            this.Text = text;
+        }
+
+        public void CallSeculityCenter(string msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeState(IState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RecordLog(string msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetClock(int hour)
+        {
+            throw new NotImplementedException();
         }
     }
 }
