fe8017202d8ce4c82c06e37f45daed95bb24f146
nodashin <rin.s.n.0314@Gmail.com>
2019-09-21 00:37:11 +0900

    Main

---
+++ DesignPatternTraning/State/Sample/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace State.Sample
+{
+    class Main
+    {
+        public static void Execute()
+        {
+            SafeForm form = new SafeForm("State Sample");
+            form.Show();
+            while(true)
+            {
+                for (int i = 0; i < 24; i++)
+                {
+                    form.SetClock(i);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
