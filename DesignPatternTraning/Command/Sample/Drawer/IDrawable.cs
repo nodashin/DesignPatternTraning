56e764989fc80e8355b3b0315e0faa81e1eddd16
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-21 16:39:36 +0900

    IDrawable中身作成。

--- DesignPatternTraning/Command/Sample/Drawer/IDrawable.cs
+++ DesignPatternTraning/Command/Sample/Drawer/IDrawable.cs
@@ -1,7 +1,15 @@
 ﻿namespace Command.Sample.Drawer
 {
+    /// <summary>
+    /// 「描画対象」を表現するインタフェース。
+    /// </summary>
     public interface IDrawable
     {
+        /// <summary>
+        /// 描画する。
+        /// </summary>
+        /// <param name="x">描画位置X軸</param>
+        /// <param name="y">描画位置Y軸</param>
         void Draw(int x, int y);
     }
 }
