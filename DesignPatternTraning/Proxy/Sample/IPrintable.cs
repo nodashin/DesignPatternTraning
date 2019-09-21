0e883cf4ffe3d9d5f93b86b10618acc37927e901
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-21 12:54:58 +0900

    IPrintable内容記述。

--- DesignPatternTraning/Proxy/Sample/IPrintable.cs
+++ DesignPatternTraning/Proxy/Sample/IPrintable.cs
@@ -1,6 +1,26 @@
 ﻿namespace Proxy.Sample
 {
+    /// <summary>
+    /// PrinterとPrinterProxyに共通のインタフェース
+    /// </summary>
     public interface IPrintable
     {
+        /// <summary>
+        /// 名前の設定。
+        /// </summary>
+        /// <param name="name">名前</param>
+        void SetPrinterName(string name);
+
+        /// <summary>
+        /// 名前の取得。
+        /// </summary>
+        /// <returns>名前</returns>
+        string GetPrinterName();
+
+        /// <summary>
+        /// プリントする。
+        /// </summary>
+        /// <param name="string">プリントする内容</param>
+        void Print(string @string);
     }
 }
