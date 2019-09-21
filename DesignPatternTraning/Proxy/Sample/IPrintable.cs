0b22080a649086a8bcf7fb2e4ae6f75633ae0967
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-21 12:53:09 +0900

    Printer、IPrintable作成。

---
+++ DesignPatternTraning/Proxy/Sample/IPrintable.cs
@@ -0,0 +1,6 @@
+﻿namespace Proxy.Sample
+{
+    public interface IPrintable
+    {
+    }
+}
---
+++ DesignPatternTraning/Proxy/Sample/Printer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Proxy.Sample
+{
+    /// <summary>
+    /// 名前つきのプリンタを表すクラス(本人)
+    /// </summary>
+    public class Printer : IPrintable
+    {
+        /// <summary>
+        /// 名前
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Printer()
+        {
+            HeavyJob("Printerのインスタンスを生成中");
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="name">名前</param>
+        public Printer(string name)
+        {
+            this.name = name;
+            HeavyJob("Printerのインスタンス(" + name + ")を生成中");
+        }
+
+        /// <summary>
+        /// 名前の設定。
+        /// </summary>
+        /// <param name="name">名前</param>
+        public void SetPrinterName(string name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// 名前の取得。
+        /// </summary>
+        /// <returns>名前</returns>
+        public string GetPrinterName()
+        {
+            return name;
+        }
+
+        /// <summary>
+        /// プリントする。
+        /// </summary>
+        /// <param name="string">プリントする内容</param>
+        public void Print(string @string)
+        {
+            Console.WriteLine("=== " + name + " ===");
+            Console.WriteLine(@string);
+        }
+
+        /// <summary>
+        /// 重い作業
+        /// </summary>
+        /// <param name="msg">メッセージ</param>
+        private void HeavyJob(string msg)
+        {
+            Console.Write(msg);
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(".");
+            }
+            Console.WriteLine("完了。");
+        }
+    }
+}
