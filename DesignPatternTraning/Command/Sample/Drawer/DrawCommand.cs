4f4f3e60f2e8e58dce9fef30e3ab4e5b0ea21ae3
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-21 16:38:21 +0900

    DrawCommand追加。

---
+++ DesignPatternTraning/Command/Sample/Drawer/DrawCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Command.Sample.Command;
+
+namespace Command.Sample.Drawer
+{
+    /// <summary>
+    /// 「点の描画命令」を表現するクラス。
+    /// </summary>
+    public class DrawCommand : ICommand
+    {
+        /// <summary>
+        /// 描画対象
+        /// </summary>
+        protected IDrawable drawable;
+
+        /// <summary>
+        /// 描画位置
+        /// </summary>
+        private Point position;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="drawable">描画対象</param>
+        /// <param name="position">描画位置</param>
+        public DrawCommand(IDrawable drawable, Point position)
+        {
+            this.drawable = drawable;
+            this.position = position;
+        }
+
+        /// <summary>
+        /// 実行
+        /// </summary>
+        public void Execute()
+        {
+            drawable.Draw(position.X, position.Y);
+        }
+    }
+}
---
+++ DesignPatternTraning/Command/Sample/Drawer/IDrawable.cs
@@ -0,0 +1,7 @@
+﻿namespace Command.Sample.Drawer
+{
+    public interface IDrawable
+    {
+        void Draw(int x, int y);
+    }
+}
