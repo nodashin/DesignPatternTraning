22663d64ad58e2daad44ef467ee93794f29f3667
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-21 16:18:36 +0900

    MacroCommand作成。 名前空間変更。

--- DesignPatternTraning/Command/Sample/Command/ICommand.cs
+++ DesignPatternTraning/Command/Sample/Command/ICommand.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Command.Sample
+namespace Command.Sample.Command
 {
     /// <summary>
     /// 「命令」を表現するインタフェース
---
+++ DesignPatternTraning/Command/Sample/Command/MacroCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Sample.Command
+{
+    /// <summary>
+    /// 「複数の命令をまとめた命令」を表現するクラス。
+    /// </summary>
+    public class MacroCommand : ICommand
+    {
+        /// <summary>
+        /// 命令の集合
+        /// </summary>
+        private Stack<ICommand> commands = new Stack<ICommand>();
+
+        /// <summary>
+        /// 命令を実行する。
+        /// </summary>
+        public void Execute()
+        {
+            while(commands.Count > 0)
+            {
+                commands.Pop().Execute();
+            }
+        }
+
+        /// <summary>
+        /// 追加。
+        /// </summary>
+        /// <param name="cmd">追加するコマンド</param>
+        public void Append(ICommand cmd)
+        {
+            if(cmd != this)
+            {
+                commands.Push(cmd);
+            }
+        }
+
+        /// <summary>
+        /// 最後の命令を削除する。
+        /// </summary>
+        public void Undo()
+        {
+            if(commands.Count != 0)
+            {
+                commands.Pop();
+            }
+        }
+
+        /// <summary>
+        /// 全部削除
+        /// </summary>
+        public void Clear()
+        {
+            commands.Clear();
+        }
+    }
+}
