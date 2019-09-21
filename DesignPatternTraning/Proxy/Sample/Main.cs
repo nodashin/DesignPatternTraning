bafccdc6332ee2877d7c06057ccd0e6d41339130
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-21 13:03:01 +0900

    Main作成。

---
+++ DesignPatternTraning/Proxy/Sample/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.Sample
+{
+    class Main
+    {
+        public static void Execute()
+        {
+            IPrintable p = new PrinterProxy("Alice");
+            Console.WriteLine("名前は現在" + p.GetPrinterName() + "です。");
+            p.SetPrinterName("Bob");
+            Console.WriteLine("名前は現在" + p.GetPrinterName() + "です。");
+            p.Print("Hello, world.");
+        }
+    }
+}
