db14299fd716b4a8599c62580edb0783ee660066
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-09-21 16:07:40 +0900

    ICommand作成。

---
+++ DesignPatternTraning/Command/Sample/ICommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Sample
+{
+    /// <summary>
+    /// 「命令」を表現するインタフェース
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// 命令を実行する。
+        /// </summary>
+        void Execute();
+    }
+}
